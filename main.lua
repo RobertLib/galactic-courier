e0b360132d4c3ce63f41abe19213b4294cdec772
RobertLib <rob@hotmail.cz>
2024-11-02 19:43:52 +0100

    Enemies

--- main.lua
+++ main.lua
@@ -1,9 +1,19 @@
+local LEVEL_WIDTH = 1600
+local LEVEL_HEIGHT = 1200
+
+local checkCollisionCircleCircle = function(x1, y1, r1, x2, y2, r2)
+  return (x1 - x2) ^ 2 + (y1 - y2) ^ 2 <= (r1 + r2) ^ 2
+end
+
+local enemies = {}
+
 local spaceship = {
   x = love.graphics.getWidth() / 2,
   y = love.graphics.getHeight() / 2,
   xVel = 0,
   yVel = 0,
   angle = -math.pi / 2,
+  radius = 10,
   speed = 100,
   vertices = { 20, 0, -10, -10, -10, 10 },
   bullets = {},
@@ -17,6 +27,13 @@ end
 function spaceship:accelerate(dt)
   self.xVel = self.xVel + math.cos(self.angle) * self.speed * dt
   self.yVel = self.yVel + math.sin(self.angle) * self.speed * dt
+
+  local speed = math.sqrt(self.xVel ^ 2 + self.yVel ^ 2)
+
+  if speed > self.speed then
+    self.xVel = self.xVel / speed * self.speed
+    self.yVel = self.yVel / speed * self.speed
+  end
 end
 
 function spaceship:move(dt)
@@ -37,6 +54,7 @@ function spaceship:shoot()
     xVel = self.xVel + math.cos(self.angle) * SPEED,
     yVel = self.yVel + math.sin(self.angle) * SPEED,
     angle = self.angle,
+    radius = 2.5,
     vertices = { 0, 0, 5, 0 }
   }
 
@@ -48,9 +66,19 @@ function spaceship:shoot()
   function bullet:update(dt)
     self.x = self.x + self.xVel * dt
     self.y = self.y + self.yVel * dt
+
+    for i, enemy in ipairs(enemies) do
+      if checkCollisionCircleCircle(
+            self.x, self.y, self.radius,
+            enemy.x, enemy.y, enemy.radius) then
+        table.remove(enemies, i)
+        break
+      end
+    end
   end
 
   function bullet:draw()
+    love.graphics.setColor(1, 1, 1)
     love.graphics.push()
     love.graphics.translate(self.x, self.y)
     love.graphics.rotate(self.angle)
@@ -79,25 +107,30 @@ function spaceship:update(dt)
 
   self:move(dt)
 
+  self.x = self.x % LEVEL_WIDTH
+  self.y = self.y % LEVEL_HEIGHT
+
   for i, bullet in ipairs(self.bullets) do
     bullet:update(dt)
 
-    if bullet.x < 0 or bullet.x > love.graphics.getWidth() or
-        bullet.y < 0 or bullet.y > love.graphics.getHeight() then
+    if bullet.x < 0 or bullet.x > LEVEL_WIDTH or
+        bullet.y < 0 or bullet.y > LEVEL_HEIGHT then
       table.remove(self.bullets, i)
     end
   end
 end
 
 function spaceship:drawThruster()
+  love.graphics.setColor(0.8, 0.8, 1)
   love.graphics.push()
   love.graphics.translate(self.x, self.y)
   love.graphics.rotate(self.angle)
-  love.graphics.polygon('line', -30 * math.random() - 5, 0, -10, -5, -10, 5)
+  love.graphics.polygon('line', -30 * love.math.random() - 5, 0, -10, -5, -10, 5)
   love.graphics.pop()
 end
 
 function spaceship:draw()
+  love.graphics.setColor(1, 1, 1)
   love.graphics.push()
   love.graphics.translate(self.x, self.y)
   love.graphics.rotate(self.angle)
@@ -113,16 +146,70 @@ function spaceship:draw()
   end
 end
 
-function love.load()
+local function spawnEnemy()
+  local enemy = {
+    x = love.math.random(LEVEL_WIDTH),
+    y = love.math.random(LEVEL_HEIGHT),
+    xVel = 0,
+    yVel = 0,
+    angle = 0,
+    radius = 10,
+    speed = 50,
+    vertices = { 0, 10, 10, 0, 0, -10, -10, 0 }
+  }
 
+  function enemy:update(dt)
+
+  end
+
+  function enemy:draw()
+    love.graphics.setColor(1, 0.8, 0.8)
+    love.graphics.push()
+    love.graphics.translate(self.x, self.y)
+    love.graphics.rotate(self.angle)
+    love.graphics.polygon('line', self.vertices)
+    love.graphics.pop()
+  end
+
+  table.insert(enemies, enemy)
+end
+
+function enemies:update(dt)
+  for _, enemy in ipairs(enemies) do
+    enemy:update(dt)
+  end
+end
+
+function enemies:draw()
+  for _, enemy in ipairs(enemies) do
+    enemy:draw()
+  end
+end
+
+function love.load()
+  love.window.setTitle('Galactic Courier')
+
+  for i = 1, 10 do
+    spawnEnemy()
+  end
 end
 
 function love.update(dt)
   spaceship:update(dt)
+  enemies:update(dt)
 end
 
 function love.draw()
+  love.graphics.translate(
+    love.graphics.getWidth() / 2 - spaceship.x,
+    love.graphics.getHeight() / 2 - spaceship.y
+  )
+
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle('line', 0, 0, LEVEL_WIDTH, LEVEL_HEIGHT)
+
   spaceship:draw()
+  enemies:draw()
 end
 
 function love.keypressed(key)
